3644ec4ffc76d268ee188cd4c592f43b396ea7c3
Mohak Bansal <117752286+Jaihanuman64@users.noreply.github.com>
2022-12-11 13:49:33 +0530

    Update 124. Binary Tree Maximum Path Sum.cpp

--- 124. Binary Tree Maximum Path Sum.cpp
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -17,3 +17,30 @@ int maxPathSum(node * root) {
   return maxi;
 
 }
+
+
+// techdose
+
+ int maxPathSum_util(TreeNode *root,int &result)
+    {
+        if(!root)
+            return 0;
+        
+        int left = maxPathSum_util(root->left,result);
+        int right = maxPathSum_util(root->right,result);
+        
+        int max_straight = max(max(left,right)+root->val, root->val);
+        int max_caseVal = max(max_straight,left+right+root->val);
+        result = max(max_caseVal,result);
+        
+        return max_straight;
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int result = INT_MIN;
+        maxPathSum_util(root,result);
+        return result; 
+    }
